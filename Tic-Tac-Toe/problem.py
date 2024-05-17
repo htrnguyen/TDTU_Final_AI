7aab220dc2670650e6e3b6b27fe91602d61932eb
nguyenn-04 <hatrongnguyen0423@gmail.com>
2024-05-17 20:12:27 +0700

    Final Tic Tac Toe

--- Tic-Tac-Toe/problem.py
+++ Tic-Tac-Toe/problem.py
@@ -2,29 +2,43 @@ import numpy as np
 import regex as re
 from board import Board
 
+
 class Problem:
+    """
+    Quản lý trạng thái của game và các hàm liên quan
+    """
+
+    """
+    UTILITY: giá trị đánh giá cho các trường hợp trên bàn cờ
+    """
     UTILITY = {
-        "Quintet": [20000000, ["xxxx"]],
-        "Quartet_2Opens": [400000, ["exxxe"]],
-        "Quartet_1Open": [50000, ["bxxxxe", "exxxxb"]],
-        "Triplet_2Opens": [30000, ["exxxe"]],
-        "Triplet_1Open": [15000, ["bxxxe", "exxxb"]],
-        "ProbQuartet_2Opens": [7000, ["exexxe", "exxexe"]],
-        "ProbQuartet_1Open": [3000, ["bxexxe", "bxxexe", "exxexb", "exexxb"]],
-        "Double_2Opens": [500, ["exxee", "eexxe"]],
-        "Double_1Open": [400, ["bxxee", "eexxb"]],
-        "Double_1Open_1Blocked": [200, ["bxxe"]],
-        "nProbTriplet_2Opens": [100, ["exexe"]],
-        "ProbTriplet_1Open": [40, ["bxexee", "eexexb"]],
+        "Quartet": [20000000, ["xxxx"]],
+        "Triplet_2Opens": [400000, ["exxxe"]],
+        "Triplet_1Open": [50000, ["bxxxe", "exxxb"]],
+        "Double_2Opens": [30000, ["exxe", "eexx"]],
+        "Double_1Open": [15000, ["bxxe", "eexb"]],
+        "ProbTriplet_2Opens": [7000, ["exexxe", "exxexe"]],
+        "ProbTriplet_1Open": [3000, ["bxexxe", "bxxexe", "exxexb", "exexxb"]],
+        "Single_2Opens": [500, ["exee", "eeex"]],
+        "Single_1Open": [400, ["bxe", "eexb"]],
+        "Single_1Open_1Blocked": [200, ["bxe"]],
+        "nProbDouble_2Opens": [100, ["exxe"]],
+        "ProbSingle_1Open": [40, ["bxeee", "eeexb"]],
     }
 
     def __init__(self, board, human_player="X"):
+        """
+        Khởi tạo trạng thái game
+        """
         self.board = board
         self.human_player = human_player
         self.ai_player = "O" if human_player == "X" else "X"
         self.current_player = self.human_player
 
     def switch_player(self):
+        """
+        Đổi lượt chơi
+        """
         self.current_player = (
             self.ai_player
             if self.current_player == self.human_player
@@ -32,28 +46,49 @@ class Problem:
         )
 
     def check_winner(self, player):
+        """
+        Kiểm tra chiến thắng của player
+        """
         lines = self.board.get_all_lines()
         return np.any(["".join(line).find(player * 4) != -1 for line in lines])
 
-    def is_goal(self):
+    def is_game_over(self):
+        """
+        Kiểm tra game kết thúc (Goal state)
+        """
         return (
             self.check_winner(self.human_player)
             or self.check_winner(self.ai_player)
             or self.board.is_full()
         )
 
+    def get_valid_moves(self):
+        """
+        Lấy tất cả các nước đi hợp lệ
+        """
+        return list(zip(*np.where(self.board.board == self.board.empty)))
+
     def evaluate(self):
+        """
+        Hàm đánh giá trạng thái game
+        """
         return self.calculate_heuristic(self.board.board, self.ai_player)
 
-    def search_in_list(self, lists, search_for):
-        search_for_compiled = re.compile(search_for)
+    def count_patterns_in_lines(self, lines, pattern):
+        """
+        Đếm số lần xuất hiện của pattern trong lines
+        """
+        compiled_pattern = re.compile(pattern)
         return sum(
-            len(search_for_compiled.findall(lst, overlapped=True)) for lst in lists
+            len(compiled_pattern.findall(line, overlapped=True)) for line in lines
         )
 
-    def make_lines(self, matrix, player):
+    def generate_lines(self, matrix, player):
+        """
+        Tạo ra tất cả các dòng, cột, đường chéo trên bàn cờ
+        """
         lines = []
-        trans = {player: "x", "-": "e"}
+        trans = {player: "x", self.board.empty: "e"}
 
         def translate(arr):
             return "".join([trans.get(c, "b") for c in arr])
@@ -70,41 +105,54 @@ class Problem:
         return lines
 
     def calculate_heuristic(self, board, player):
-        def get_sequence_heuristic(lines):
-            sequence_heuristic = 0
-            for key, (value, patterns) in self.UTILITY.items():
+        """
+        Tính giá trị heuristic của trạng thái game
+        """
+
+        def get_sequence_score(lines):
+            """
+            Tính giá trị heuristic dựa trên các pattern
+            """
+            sequence_score = 0
+            for _, (value, patterns) in self.UTILITY.items():
                 for pattern in patterns:
-                    sequence_heuristic += value * self.search_in_list(lines, pattern)
-            return sequence_heuristic
+                    sequence_score += value * self.count_patterns_in_lines(
+                        lines, pattern
+                    )
+            return sequence_score
 
-        def get_position_heuristic(board, player):
+        def get_position_score(board, player):
+            """
+            Tính giá trị heuristic dựa trên vị trí của các quân cờ
+            """
             player_board = np.where(board == player, 1, 0)
-            return np.sum(player_board * Board.HEURISTIC)
+            return np.sum(player_board * self.board.HEURISTIC)
 
-        lines = self.make_lines(board, player)
-        player_score = get_sequence_heuristic(lines) + get_position_heuristic(
-            board, player
-        )
+        lines = self.generate_lines(board, player)
+        player_score = get_sequence_score(lines) + get_position_score(board, player)
 
         opponent = "O" if player == "X" else "X"
-        lines_opponent = self.make_lines(board, opponent)
-        opponent_score = get_sequence_heuristic(
-            lines_opponent
-        ) + get_position_heuristic(board, opponent)
+        opponent_lines = self.generate_lines(board, opponent)
+        opponent_score = get_sequence_score(opponent_lines) + get_position_score(
+            board, opponent
+        )
 
         return player_score - 1.05 * opponent_score
 
     def evaluate_move(self, move):
+        """
+        Đánh giá nước đi
+        """
         x, y = move
         temp_board = self.board.board.copy()
         temp_board[x][y] = self.ai_player
         score = self.calculate_heuristic(temp_board, self.ai_player)
         return score
 
-    def get_valid_moves(self):
-        return list(zip(*np.where(self.board.board == "-")))
-
     def sort_moves(self):
+        """
+        Sắp xếp các nước đi theo thứ tự gần tâm bàn cờ
+        """
         center = self.board.size // 2
         valid_moves = self.get_valid_moves()
         return sorted(

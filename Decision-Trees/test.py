021696eb46699597369087e0677cabaa0bee2789
Luong Chi Trung <luongchitrung53@gmail.com>
2024-05-17 18:21:40 +0700

    complete 3.1

--- Decision-Trees/test.py
+++ Decision-Trees/test.py
@@ -2,4 +2,6 @@ from problem import Problem
 
 p = Problem('dt_data.csv', 'Rank', ['#'])
 
-print(p.get_entropy_attribute('Q1'))
+print(p.get_H_attribute('Q2'))
+print(p.get_AE_attribute('Q2'))
+print(p.get_IG_attribute('Q2'))

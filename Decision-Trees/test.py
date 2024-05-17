596b342bfc79386daed0d8c6826f344093e0960f
Luong Chi Trung <luongchitrung53@gmail.com>
2024-05-17 19:10:46 +0700

    change input of Problem

--- Decision-Trees/test.py
+++ Decision-Trees/test.py
@@ -1,6 +1,8 @@
 from problem import Problem
+import pandas as pd
 
-p = Problem('dt_data.csv', 'Rank', ['#'])
+df = pd.read_csv('./dt_data.csv')
+p = Problem(df, 'Rank', ['#'])
 
 print(p.get_H_attribute('Q2'))
 print(p.get_AE_attribute('Q2'))

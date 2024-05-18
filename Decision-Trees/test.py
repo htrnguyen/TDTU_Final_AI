0da7062ebbe43cdf9703b4b5d3ec7cde7d41d09e
Luong Chi Trung <luongchitrung53@gmail.com>
2024-05-18 17:14:41 +0700

    build model with CART and C4.5 algorithm

--- Decision-Trees/test.py
+++ Decision-Trees/test.py
@@ -4,6 +4,6 @@ import pandas as pd
 df = pd.read_csv('./dt_data.csv')
 p = Problem(df, 'Rank', ['#'])
 
-print(p.get_H_attribute('Q2'))
-print(p.get_AE_attribute('Q2'))
-print(p.get_IG_attribute('Q2'))
+print(p.get_H_attribute('Q1'))
+print(p.get_AE_attribute('Q1'))
+print(p.get_IG_attribute('Q1'))
